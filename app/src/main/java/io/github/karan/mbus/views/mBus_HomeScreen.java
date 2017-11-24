7f98fa3b7af3ee75741e40a7a63171616f0937b3
karan1525 <karan1525@yahoo.co.in>
2017-11-24 00:02:40 -0800

    Initial project setup and splash screen added

---
+++ app/src/main/java/io/github/karan/mbus/views/mBus_HomeScreen.java
@@ -0,0 +1,15 @@
+package io.github.karan.mbus.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.karan.mbus.R;
+
+public class mBus_HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_bus__home_screen);
+    }
+}
---
+++ app/src/main/java/io/github/karan/mbus/views/mBus_SplashActivity.java
@@ -0,0 +1,20 @@
+package io.github.karan.mbus.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A class representing the splash screen
+ */
+public class mBus_SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, mBus_HomeScreen.class);
+        startActivity(intent);
+        finish();
+    }
+}
